07125c54d8b3a41bdd240e855ace215f7cb85566
Miguel Leite <miguelleite200leite@gmail.com>
2024-10-22 01:19:21 +0100

    🐳 chore: adds onboarding screen with animations

--- declarations.d.ts
+++ declarations.d.ts
@@ -4,3 +4,24 @@ declare module "*.svg" {
   const content: React.FC<SvgProps>;
   export default content;
 }
+
+declare module "*.png" {
+  import React from "react";
+  import { SvgProps } from "react-native-svg";
+  const content: React.FC<SvgProps>;
+  export default content;
+}
+
+declare module "*.jpg" {
+  import React from "react";
+  import { SvgProps } from "react-native-svg";
+  const content: React.FC<SvgProps>;
+  export default content;
+}
+
+declare module "*.jpeg" {
+  import React from "react";
+  import { SvgProps } from "react-native-svg";
+  const content: React.FC<SvgProps>;
+  export default content;
+}
